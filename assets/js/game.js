522b583477490ab42d2e5ccfae01a1282f99f130
Mitchell Frey <mfcodingbc@gmail.com>
2022-02-17 18:18:04 -0500

    Player Data 1st Function Created

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,19 @@
-window.alert("This is an alert! JavaScript is running!");
+//This creates a user prompt wherein a user inputs their data (robot name in this case)
+//The variable 'playerName' will contain the input and will be called when utilized later in code
+
+var playerName = window.prompt("What is your robot's name?");
+console.log("This logs a string, good for leaving yourself a message");
+// this will do math and log 20
+console.log(10+10);
+console.log("Our robot's name is " + playerName);
+// window.alert(playerName);
+//Alerts are for the user, not necessarily for developer (limit alerts for user)
+//
+
+// This creates a function name "fight"
+
+function fight() {
+    window.alert("The fight has begun!");
+}
+
+// fight();
