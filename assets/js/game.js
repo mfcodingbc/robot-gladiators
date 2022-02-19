23cb73985def624bcd81948dfad98721ac9c75cf
Mitchell Frey <mfcodingbc@gmail.com>
2022-02-18 20:15:23 -0500

    multiple rounds complete

--- assets/js/game.js
+++ assets/js/game.js
@@ -13,9 +13,6 @@ var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-// A fight alert letting users know they are about to start a fight
-window.alert("The battle is about to begin!");
-
 var fight = function(enemyName) {
     while (playerHealth > 0 && enemyHealth > 0) {
         // ask player if they'd like to fight or run
@@ -78,8 +75,23 @@ var fight = function(enemyName) {
 
 //This runs the code in the page:
 for(var i = 0; i < enemyNames.length; i++) {
-    var pickedEnemyName = enemyNames [i];
-    enemyHealth = 50;
-    // call fight function with enemy-robot
-    fight(pickedEnemyName);
+    if (playerHealth > 0) {
+        // let player know what round they are in, remember that arrays start at 0 so it needs to have 1 added to it
+        window.alert("Prepare for battle! Round " + (i + 1) + " is about to begin!");
+    
+        // pick new enemy to fight based on the index of the enemyNames array
+        var pickedEnemyName = enemyNames[i];
+    
+        // reset enemyHealth before starting new fight
+        enemyHealth = 50;
+    
+        // use debugger to pause script from running and check what's going on at that moment in the code
+        // debugger;
+    
+        // pass the pickedEnemyName variable's value into the fight function, where it will assume the value of the enemyName parameter
+        fight(pickedEnemyName);
+    }
+    else {
+        window.alert("You have lost your robot in battle! Game Over!");
+    }
 }
